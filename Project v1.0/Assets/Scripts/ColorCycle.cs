dba6cdab2b478d330cfb7040ec8ab42e0e499f59
Xun <47920917+Oscarxun@users.noreply.github.com>
2019-02-28 14:00:28 +0800

    ver1.0

---
+++ Project v1.0/Assets/Scripts/ColorCycle.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorCycle : MonoBehaviour
+{
+    private Renderer rend;
+    //public GameObject timer;
+    //private Timer timerScript;
+
+    public Color[] TopColor;
+    public Color[] BottomColor;
+    public bool isChanging = true;
+    private int currentIndex;
+    private int maxIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentIndex = 0;
+        maxIndex = TopColor.Length;
+        //timerScript = timer.GetComponent<Timer>();
+        rend = GetComponent<Renderer>();
+        rend.material.SetColor("_Color1", TopColor[currentIndex]);
+        rend.material.SetColor("_Color", BottomColor[currentIndex]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isChanging)
+        {
+            Color startTopColor = rend.material.GetColor("_Color1");
+            Color startBottomColor = rend.material.GetColor("_Color");
+
+            Color endTopColor = TopColor[0];
+            Color endBottomColor = BottomColor[0];
+
+            if (currentIndex + 1 < maxIndex)
+            {
+                endTopColor = TopColor[currentIndex + 1];
+                endBottomColor = BottomColor[currentIndex + 1];
+            }
+
+            Color newTopColor = Color.Lerp(startTopColor, endTopColor, Time.deltaTime);
+            Color newBottomColor = Color.Lerp(startBottomColor, endBottomColor, Time.deltaTime);
+
+            rend.material.SetColor("_Color1", newTopColor);
+            rend.material.SetColor("_Color", newBottomColor);
+
+            if(newTopColor == endTopColor && newBottomColor == endBottomColor)
+            {
+                //isChanging = false;
+
+                if (currentIndex + 1 < maxIndex)
+                {
+                    currentIndex++;
+                }
+                else
+                {
+                    currentIndex = 0;
+                }
+            }
+        }
+        
+
+    }
+}
---
+++ Project v1.0/Assets/Scripts/Timer.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public Image timer;
+    public float currentAmount;
+    public float speed;
+    public float speedMultiplier;
+    public int stage;
+    public TextMeshProUGUI stageText;
+
+    public bool starting;
+    public bool nextStage;
+    public bool startCountdown;
+    public bool nothing;
+    public bool lose;
+    public static bool tutorial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        stage = 0;
+        stageText.GetComponent<TextMeshProUGUI>().text = stage.ToString();
+        timer = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!lose)
+        {
+            if (currentAmount > 0 && startCountdown && !starting)
+            {
+                //Countdown timer
+                currentAmount -= speed * Time.deltaTime;
+
+                //Change timer color
+                if (currentAmount <= 35 && currentAmount > 20)
+                {
+                    timer.color = new Color(1.0f, 0.64f, 0.0f);
+                }
+                else if(currentAmount <= 20)
+                {
+                    timer.color = Color.red;
+                }
+
+                //lose condition - time's up
+                if (currentAmount <= 0 && !nothing)
+                {
+                    lose = true;
+                }
+            }
+            else if (nextStage)
+            {
+                stage++;
+                stageText.GetComponent<TextMeshProUGUI>().text = stage.ToString();
+                currentAmount = 100;
+                timer.color = Color.white;
+                if (speed <= 150 && !tutorial) //max speed = 150
+                    speed *= speedMultiplier;
+                nextStage = false;
+            }
+
+            timer.fillAmount = currentAmount / 100;
+        }
+    }
+    
+}
