9524049d6140ef507a4a26e800f973f0677d976c
Xun <47920917+Oscarxun@users.noreply.github.com>
2019-02-28 13:20:39 +0800

    ver0.1

---
+++ Project v0.1/Assets/Scripts/GameController.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject timer;
+    private Timer timerScript;
+
+    public GameObject retryPanel;
+
+    public GameObject swipe;
+    private SwipeDetection swipeScript;
+
+    public TextMeshProUGUI orderText;
+    private string[] orders = { "UP", "DOWN", "LEFT", "RIGHT", "TAP", "DOUBLE TAP", "HOLD" };
+    public int randIndex;
+    public string randOrder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerScript = timer.GetComponent<Timer>();
+        swipeScript = swipe.GetComponent<SwipeDetection>();
+
+        timerScript.starting = true;
+        timerScript.nextStage = false;
+        timerScript.lose = false;
+        orderText.GetComponent<TextMeshProUGUI>().text = "TAP TO START";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timerScript.starting)
+        {
+            if(swipeScript.Tap)
+            {
+                randOrder = RandomOrder();
+                orderText.GetComponent<TextMeshProUGUI>().text = randOrder;
+                timerScript.starting = false;
+            }
+        }
+
+        if (!timerScript.lose)
+        {
+            switch(randOrder)
+            {
+                case "UP":
+                    {
+                        if (swipeScript.SwipeUp)
+                        {
+                            timerScript.nextStage = true;
+                        }
+                        else if (swipeScript.SwipeDown || swipeScript.SwipeLeft || swipeScript.SwipeRight)
+                        {
+                            timerScript.lose = true;
+                        }
+                        break;
+                    }
+
+                case "DOWN":
+                    {
+                        if (swipeScript.SwipeDown)
+                        {
+                            timerScript.nextStage = true;
+                        }
+                        else if (swipeScript.SwipeUp || swipeScript.SwipeLeft || swipeScript.SwipeRight)
+                        {
+                            timerScript.lose = true;
+                        }
+                        break;
+                    }
+
+                case "LEFT":
+                    {
+                        if (swipeScript.SwipeLeft)
+                        {
+                            timerScript.nextStage = true;
+                        }
+                        else if (swipeScript.SwipeUp || swipeScript.SwipeDown || swipeScript.SwipeRight)
+                        {
+                            timerScript.lose = true;
+                        }
+                        break;
+                    }
+
+                case "RIGHT":
+                    {
+                        if (swipeScript.SwipeRight)
+                        {
+                            timerScript.nextStage = true;
+                        }
+                        else if (swipeScript.SwipeUp || swipeScript.SwipeLeft || swipeScript.SwipeDown)
+                        {
+                            timerScript.lose = true;
+                        }
+                        break;
+                    }
+
+                case "TAP":
+                    {
+                        if (swipeScript.Tap)
+                        {
+                            timerScript.nextStage = true;  
+                        }
+                        else if (swipeScript.SwipeUp || swipeScript.SwipeLeft || swipeScript.SwipeDown || swipeScript.SwipeRight)
+                        {
+                            timerScript.lose = true;
+                        }
+                        break;
+                    }
+
+                case "DOUBLE TAP":
+                    {
+                        if (swipeScript.DoubleTap)
+                        {
+                            timerScript.nextStage = true;
+                        }
+                        else if (swipeScript.SwipeUp || swipeScript.SwipeLeft || swipeScript.SwipeDown || swipeScript.SwipeRight)
+                        {
+                            timerScript.lose = true;
+                        }
+                        break;
+                    }
+
+                case "HOLD":
+                    {
+                        if(swipeScript.Hold)
+                        {
+                            timerScript.nextStage = true;
+                        }
+                        else if (swipeScript.SwipeUp || swipeScript.SwipeLeft || swipeScript.SwipeDown || swipeScript.SwipeRight)
+                        {
+                            timerScript.lose = true;
+                        }
+                        break;
+                    }
+            }
+
+            if (timerScript.nextStage)
+            {
+                //if (timerScript.stage > 20)
+                randOrder = RandomOrder();
+                orderText.GetComponent<TextMeshProUGUI>().text = randOrder;
+            }
+        }
+
+            if (timerScript.lose)
+        {
+            orderText.GetComponent<TextMeshProUGUI>().text = "YOU LOSE!";
+            Time.timeScale = 0;
+            retryPanel.gameObject.SetActive(true);
+        }
+    }
+
+    public string RandomOrder()
+    {
+        randIndex = Random.Range(0, orders.Length);
+        return orders[randIndex];
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Gameplay");
+        Time.timeScale = 1;
+        retryPanel.gameObject.SetActive(false);
+    }
+}
---
+++ Project v0.1/Assets/Scripts/SwipeDetection.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeDetection : MonoBehaviour
+{
+
+    private bool tap, doubleTap, hold, swipeLeft, swipeRight, swipeUp, swipeDown;
+    private bool isDragging = false;
+    private Vector2 startTouch, swipeDelta;
+
+    private float holdTime = 0.3f;
+    private float startHoldTime;
+    private float tapDelay = 0.5f;
+    private float tapTime;
+    private int tapCount = 0;
+
+    public Vector2 SwipeDelta { get { return swipeDelta; } }
+    public bool SwipeLeft { get { return swipeLeft;  } }
+    public bool SwipeRight { get { return swipeRight; } }
+    public bool SwipeUp { get { return swipeUp; } }
+    public bool SwipeDown { get { return swipeDown; } }
+    public bool Tap { get { return tap; } }
+    public bool DoubleTap { get { return doubleTap; } }
+    public bool Hold { get { return hold; } }
+
+    public float swipeRange;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        tap = doubleTap = hold = swipeLeft = swipeRight = swipeUp = swipeDown = false;
+
+         #region Mouse Inputs
+        if(Input.GetMouseButtonDown(0))
+        {
+            startHoldTime = Time.time;
+            tap = true;
+            isDragging = true;
+            startTouch = Input.mousePosition;
+            tapCount++;
+            if (tapCount == 1)
+                tapTime = Time.time;
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+            Reset();
+        }
+
+        //Debug.Log(Time.time);
+
+        if((Time.time - startHoldTime) > holdTime)
+        {
+            if((Vector2)Input.mousePosition == startTouch)
+                hold = true;
+        }
+
+        #endregion
+
+        #region Mobile Inputs
+        if (Input.touches.Length > 0)
+        {
+            if(Input.touches[0].phase == TouchPhase.Began)
+            {
+                startHoldTime = Time.time;
+                tap = true;
+                isDragging = true;
+                startTouch = Input.touches[0].position;
+                tapCount++;
+                if (tapCount == 1)
+                    tapTime = Time.time;
+            }
+            else if(Input.touches[0].phase == TouchPhase.Ended || Input.touches[0].phase == TouchPhase.Canceled)
+            {
+                isDragging = false;
+                Reset();
+            }
+
+            if ((Time.time - startHoldTime) > holdTime)
+            {
+                if (Input.touches[0].position == startTouch)
+                    hold = true;
+            }
+        }
+        #endregion
+
+        if (tapCount > 1 && Time.time - tapTime < tapDelay)
+        {
+            tapCount = 0;
+            tapTime = 0;
+            doubleTap = true;
+        }
+        else if (tapCount > 2 || Time.time - tapTime > 1)
+            tapCount = 0;
+
+
+        swipeDelta = Vector2.zero;
+        if(startTouch != Vector2.zero)
+        {
+            if(isDragging)
+            {
+                if (Input.touches.Length > 0)
+                    swipeDelta = Input.touches[0].position - startTouch;
+                else if (Input.GetMouseButton(0))
+                    swipeDelta = (Vector2)Input.mousePosition - startTouch;
+            }
+
+            if (swipeDelta.magnitude > swipeRange)
+            {
+                //Check swipe direction
+                float x = swipeDelta.x;
+                float y = swipeDelta.y;
+                if(Mathf.Abs(x) > Mathf.Abs(y))
+                {
+                    //Left or RIght
+                    if(x < 0)
+                    {
+                        swipeLeft = true;
+                        //Debug.Log("Swipe left");
+                    }
+                    else
+                    {
+                        swipeRight = true;
+                        //Debug.Log("Swipe right");
+                    }
+                }
+                else
+                {
+                    //Up or Down
+                    if (y < 0)
+                    {
+                        swipeDown = true;
+                        //Debug.Log("Swipe down");
+                    }
+                    else
+                    {
+                        swipeUp = true;
+                        //Debug.Log("Swipe up");
+                    }
+                }
+                Reset();
+            }
+        }
+    }
+
+    private void Reset()
+    {
+        startTouch = swipeDelta = Vector2.zero;
+    }
+}
